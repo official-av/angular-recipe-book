16edf2c6702cd7b649b42f996b5ee65703d8c683
Anmol Vashistha <anmolvashisthaav@gmail.com>
2017-07-06 19:20:39 +0530

    Inter Components Communication & custom events
    Added functionality to navbar
    added events for recipes and shopping-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter,Output } from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -8,8 +8,9 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 	recipes:Recipe[]=[
-		new Recipe('A Test Recipe','Random recipe','http://img.sndimg.com/food/image/upload/h_465,w_620,c_fit/v1/img/recipes/14/21/56/7bRqI0ARdiElWXannur9_Mexican-Wrapped-Hot-Dog-4.png'),new Recipe('Not Hot Dog','see it isnt','yadyada')
+		new Recipe('Hot Dog','This is a HotDog','http://img.sndimg.com/food/image/upload/h_465,w_620,c_fit/v1/img/recipes/14/21/56/7bRqI0ARdiElWXannur9_Mexican-Wrapped-Hot-Dog-4.png'),new Recipe('Not Hot Dog','This is not a HotDog','http://media.istockphoto.com/photos/supreme-pizza-slice-lift-picture-id153784617?k=6&m=153784617&s=612x612&w=0&h=dYsMeY0ps3uiG9kq1aR7VowCqfrQaZM-XHBVYg8siNk=')
 	];
+	@Output() fireRecipe=new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
@@ -17,5 +18,8 @@ export class RecipeListComponent implements OnInit {
 //	addRecipe(a,b,c){
 //		this.recipes.push(new Recipe(a,b,c));
 //	}
-
+	onRecipeSelected(item:Recipe){
+		this.fireRecipe.emit(item);
+	}
+	
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,ViewChild,EventEmitter,Output, ElementRef } from '@angular/core';
+import {ingredient} from '../../shared/ingredient.model'
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+	@ViewChild('nameInput') nameI:ElementRef;
+	@ViewChild('amountInput') amountI:ElementRef;
+	@Output() addIngredient=new EventEmitter<ingredient>();
   constructor() { }
 
   ngOnInit() {
   }
 
+	onAdd(){
+		this.addIngredient.emit(new ingredient(this.nameI.nativeElement.value,this.amountI.nativeElement.value));
+	}
 }
